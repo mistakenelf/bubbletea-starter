611fdda8d08663f9afee61efebdc9a5e29f6a53c
knipferrc <knipferrc@gmail.com>
2021-09-18 09:49:11 -0400

    move keys to its own file

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -2,43 +2,10 @@ package ui
 
 import (
 	"github.com/charmbracelet/bubbles/help"
-	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/viewport"
 	"github.com/charmbracelet/lipgloss"
 )
-
-// keyMap struct contains all keybindings.
-type keyMap struct {
-	Help key.Binding
-	Quit key.Binding
-}
-
-// keys represents the key bindings in the app along with the help text.
-var keys = keyMap{
-	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "toggle help"),
-	),
-	Quit: key.NewBinding(
-		key.WithKeys("q", "esc", "ctrl+c"),
-		key.WithHelp("q", "quit"),
-	),
-}
-
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Help, k.Quit}
-}
-
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
-func (k keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{k.Help, k.Quit},
-	}
-}
 
 // Model represents the state of the UI.
 type Model struct {
@@ -51,6 +18,8 @@ type Model struct {
 
 // NewModel creates an instance of the UI.
 func NewModel() Model {
+	keys := getDefaultKeyMap()
+
 	l := spinner.NewModel()
 	l.Spinner = spinner.Dot
 
@@ -58,14 +27,11 @@ func NewModel() Model {
 	h.Styles.FullKey.Foreground(lipgloss.Color("#ffffff"))
 	h.Styles.FullDesc.Foreground(lipgloss.Color("#ffffff"))
 
-	v := viewport.Model{}
-	v.SetContent("Welcome to the bubbletea-starter app")
-
 	return Model{
 		keys:     keys,
 		help:     h,
 		loader:   l,
-		viewport: v,
+		viewport: viewport.Model{},
 		ready:    false,
 	}
 }
