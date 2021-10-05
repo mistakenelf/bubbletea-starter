f1d8458e8dce423e21b2d882b4049dc5b34227f5
knipferrc <knipferrc@gmail.com>
2021-10-04 20:53:01 -0400

    setup app config

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/knipferrc/bubbletea-starter/internal/constants"
+	"github.com/knipferrc/bubbletea-starter/internal/config"
 	"github.com/knipferrc/bubbletea-starter/internal/ui"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -15,14 +15,40 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "bubbletea-starter",
 	Short:   "bubbletea-starter is a starting point for bubbletea apps",
-	Version: constants.Versions.AppVersion,
+	Version: "0.0.1",
 	Args:    cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		config.SetDefaults()
+		config.LoadConfig()
+
+		cfg := config.GetConfig()
+
+		// If logging is enabled, logs will be output to debug.log.
+		if cfg.Settings.EnableLogging {
+			f, err := tea.LogToFile("debug.log", "debug")
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+
+			defer func() {
+				if err = f.Close(); err != nil {
+					log.Fatal(err)
+					os.Exit(1)
+				}
+			}()
+		}
+
 		m := ui.NewModel()
 		var opts []tea.ProgramOption
 
 		// Always append alt screen program option.
 		opts = append(opts, tea.WithAltScreen())
+
+		// If mousewheel is enabled, append it to the program options.
+		if cfg.Settings.EnableMouseWheel {
+			opts = append(opts, tea.WithMouseAllMotion())
+		}
 
 		// Initialize new app.
 		p := tea.NewProgram(m, opts...)
--- internal/ui/model.go
+++ internal/ui/model.go
@@ -5,19 +5,22 @@ import (
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/viewport"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/knipferrc/bubbletea-starter/internal/config"
 )
 
 // Model represents the state of the UI.
 type Model struct {
-	keys     keyMap
-	help     help.Model
-	loader   spinner.Model
-	viewport viewport.Model
-	ready    bool
+	keys      keyMap
+	help      help.Model
+	loader    spinner.Model
+	viewport  viewport.Model
+	appConfig config.Config
+	ready     bool
 }
 
 // NewModel creates an instance of the UI.
 func NewModel() Model {
+	cfg := config.GetConfig()
 	keys := getDefaultKeyMap()
 
 	l := spinner.NewModel()
@@ -28,10 +31,11 @@ func NewModel() Model {
 	h.Styles.FullDesc.Foreground(lipgloss.Color("#ffffff"))
 
 	return Model{
-		keys:     keys,
-		help:     h,
-		loader:   l,
-		viewport: viewport.Model{},
-		ready:    false,
+		keys:      keys,
+		help:      h,
+		loader:    l,
+		viewport:  viewport.Model{},
+		appConfig: cfg,
+		ready:     false,
 	}
 }
